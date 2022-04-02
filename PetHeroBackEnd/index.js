f2d06bfc21d6b3b53e37d6c96c6f1c4fcbe4ecd5
bog1200 <26150024+bog1200@users.noreply.github.com>
2022-04-02 21:21:53 +0300

    Post + Message APIs

--- PetHeroBackEnd/index.js
+++ PetHeroBackEnd/index.js
@@ -12,7 +12,7 @@ app.get('/ping', (req, res) => {
 
 app.post('/users/register',jsonParser, async (req, res) => {
     console.log(req);
-    const account = await db.query(`SELECT * FROM users WHERE email = "${req.body.email}"`);
+    const account = await db.query(`SELECT userid FROM users WHERE email = "${req.body.email}"`);
     if (account.length>0) {
        res.contentType('application/json').status(403).send({"error":"Email already exists"});
     }
@@ -22,19 +22,19 @@ app.post('/users/register',jsonParser, async (req, res) => {
             db.query(`INSERT INTO users (email, name, password) VALUES ("${req.body.email}", "${req.body.name}", "${hash}")`);
         });
     });
-    res.contentType('application/json').send({"message":"User registered."});
+    res.contentType('application/json').status(201).send({"message":"User registered."});
     }
 });
 app.post('/users/login', jsonParser, async (req, res) => {
-     console.log(req);
-    const accounts = await db.query(`SELECT * FROM users WHERE email = "${req.body.email}"`);
+     console.log(req)
+    const accounts = await db.query(`SELECT password, userid FROM users WHERE email = "${req.body.email}"`);
     if (accounts.length>0) {
         const account=accounts[0];
         bcrypt.compare(req.body.password, account.password, function(err, result) {
             if (result) {
                 const session = uuid();
                 db.query(`INSERT INTO sessions (token, userid) VALUES ("${session}", "${account.userid}")`);
-                res.contentType('application/json').send({"token": session});   //send the token
+                res.contentType('application/json').send({"token": session, "expiration_time":Date.now()+3600000});   //send the token
             }
         });
     }
@@ -44,7 +44,7 @@ app.post('/users/login', jsonParser, async (req, res) => {
 });
 app.post('/users/logout', jsonParser, async (req, res) => {
     console.log(req);
-    const session = await db.query(`SELECT * FROM sessions WHERE token = "${req.body.token}"`);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
     if (session.length>0) {
         db.query(`DELETE FROM sessions WHERE token = "${req.body.token}"`);
         res.contentType('application/json').send({"message":"User logged out."});
@@ -53,5 +53,122 @@ app.post('/users/logout', jsonParser, async (req, res) => {
         res.contentType('application/json').status(401).send({"error":"User not logged in."});
     }
 });
+app.post('/users/delete', jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const account = await db.query(`SELECT userid FROM users WHERE userid = "${session[0].userid}"`);
+        if (account.length>0) {
+            db.query(`DELETE FROM users WHERE userid = "${session[0].userid}"`);
+            db.query(`DELETE FROM sessions WHERE userid = "${session[0].userid}"`);
+            res.contentType('application/json').send({"message":"User deleted."});
+        }
+        else {
+            res.contentType('application/json').status(400).send({"error":"User not found."});
+        }
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+
+app.post("/posts/new", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        await db.query(`INSERT INTO posts (userid, title, content, type, race) VALUES ("${session[0].userid}", "${req.body.title}", "${req.body.content}", "${req.body.type}","${req.body.race}")`);
+        res.contentType('application/json').status(201).send({"message":"Post created."});
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+app.post("/posts/delete", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const post = await db.query(`SELECT userid FROM posts WHERE postid = ${req.body.postid}`);
+        if (post.length>0 && post[0].userid==session[0].userid) {
+            await db.query(`DELETE FROM posts WHERE postid = ${req.body.postid}`);
+            res.contentType('application/json').send({"message":"Post deleted."});
+        }
+        else {
+            res.contentType('application/json').status(401).send({"error":"User not authorized or invalid post."});
+        }
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+app.post("/posts/edit", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const post = await db.query(`SELECT postid FROM posts WHERE postid = ${req.body.postid} AND userid = "${session[0].userid}"`);
+        if (post.length>0) {
+            await db.query(`UPDATE posts SET title = "${req.body.title}", content = "${req.body.content}" WHERE postid = "${req.body.postid}"`);
+            res.contentType('application/json').send({"message":"Post edited."});
+        }
+        else{
+            res.contentType('application/json').status(401).send({"error":"User not authorized or invalid post."});
+        }
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+app.post("/posts/get", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const post = await db.query(`SELECT * FROM posts WHERE postid = "${req.body.postid}" AND userid = "${session[0].userid}"`);
+        res.contentType('application/json').send({"title":post[0].title, "content":post[0].content});
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+app.post("/posts/getall", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const posts = await db.query(`SELECT * FROM posts ORDER BY postid DESC LIMIT 10`);
+        res.contentType('application/json').send(posts);
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+
+app.post("/messages/new", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        await db.query(`INSERT INTO messages (senderid, receiverid, content) VALUES ("${session[0].userid}",${req.body.receiver} "${req.body.content}")`);
+        res.contentType('application/json').status(201).send({"message":"Message sent."});
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+
+app.post("/messages/get", jsonParser, async (req, res) => {
+    console.log(req);
+    const session = await db.query(`SELECT userid FROM sessions WHERE token = "${req.body.token}"`);
+    if (session.length>0) {
+        const messages = await db.query(`SELECT * FROM messages WHERE senderid = "${session[0].userid}" OR receiverid = "${session[0].userid}" ORDER BY messageid DESC LIMIT 10`);
+        res.contentType('application/json').send(messages);
+    }
+    else {
+        res.contentType('application/json').status(401).send({"error":"User not logged in."});
+    }
+});
+
+// app.post("*", (req, res) => {   //catch all requests
+//     res.contentType('application/json').status(401).send({"error":"Not found."});
+// });
+// app.get("*", (req, res) => {   //catch all requests
+//     res.contentType('application/json').status(401).send({"error":"Not found."});
+// });
 app.listen(5000);
 
