eea85997597a3eccefeb14a8b7b688c136566202
bog1200 <26150024+bog1200@users.noreply.github.com>
2022-04-03 12:49:57 +0300

    Name register

--- PetHeroWeb/register.js
+++ PetHeroWeb/register.js
@@ -2,7 +2,7 @@ async function register (){
   const headers = new Headers();
   headers.append('content-type', 'application/json');
 
-const body = `{"email": "${document.getElementById("email").value}", "password": "${document.getElementById("password").value}"}`;
+const body = `{"email": "${document.getElementById("email").value}","name":${document.getElementById("name").value} "password": "${document.getElementById("password").value}"}`;
 
 const init = {
 method: 'POST',
@@ -13,7 +13,7 @@ body
 const response = await fetch('https://pethero22.romail.app/api/users/register', init)
 const data = await response.json();
   if(data.message){
-    alert("Account created succesfully");
+    alert(`Account created succesfully. Hello ${document.getElementById("name").value}!`);
     window.location.href = "/login.html";
 }
 else{
